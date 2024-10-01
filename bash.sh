8ea2b2421f5218f3cf690bb64dc6dc141cf1508f
jad issa <83015351+jisoz@users.noreply.github.com>
2024-10-01 16:30:43 +0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,37 @@
+########### begin###############
+# in linux -> echo $SHELL -> /bin/bash   if not -> which bash -> /usr/bin/bash and press enter 
+
+#!/bin/bash
+
+myname="jad"
+mynum=123
+now=$(date)
+echo "hello , my name is $myname"
+echo "the system date is: $now" 
+
+if [! $mynum -eq 200 ]  or  ->  [ $mynum -ne 200]  // not equal
+then 
+    echo "true"
+else
+    echo "false"
+fi    
+
+
+# check file if exist 
+if [-f ~/file.txt] .....
+
+# check directory 
+dir=/etc
+if [-d $dir] ...
+
+package=htop 
+sudo apt install $package
+if [ $? -eq 0 ]
+then 
+   echo " the installation succsufull"
+   which $package
+else 
+   echo "failed"
+
+fi 
 
